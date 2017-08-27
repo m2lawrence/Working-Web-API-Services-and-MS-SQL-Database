203c77c49a411024635b1fb3980aea71bc4bd9ab
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2017-08-27 21:18:33 +0100

    Add files via upload

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (mikeEntities entities = new mikeEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (mikeEntities entities = new mikeEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
+
